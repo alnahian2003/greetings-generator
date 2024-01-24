89648b1446aa2cd3da184c1bdb316184f4db1fa7
Al Nahian <a.alnahian2003@gmail.com>
2024-01-25 02:35:21 +0600

    multilingual support added

--- src/Greetings.php
+++ src/Greetings.php
@@ -4,10 +4,19 @@ namespace Alnahian2003\GreetingsGenerator;
 
 class Greetings
 {
-    private static $messages = ['Hi', 'Hello', 'How Are You'];
+    private static $greetings = [
+        'bn' => ['হ্যালো', 'কেমন আছেন?', 'আসসালামু আলাইকুম'], // Bengali
+        'en' => ['Hi', 'How are you?', 'Hello'], // English
+        'ar' => ['مرحبا', 'كيف حالك؟', 'السلام عليكم'], //  Arabic
+        'tr' => ['Merhaba', 'Nasılsınız?', 'Selam'], // Turkish
+        'es' => ['Hola', '¿Cómo estás?', 'Saludos'], // Spanish
+    ];
 
-    public static function greet()
+    public static function greet(?string $language = null)
     {
-        return self::$messages[array_rand(self::$messages)];
+        $selectedLanguage = $language ?? array_rand(self::$greetings);
+        $selectedGreetings = self::$greetings[$selectedLanguage] ?? self::$greetings['en'];
+
+        return $selectedGreetings[array_rand($selectedGreetings)];
     }
 }
--- tests/GreetingsTest.php
+++ tests/GreetingsTest.php
@@ -9,7 +9,19 @@ class GreetingsTest extends TestCase
 {
     public function test_generates_random_greeting()
     {
-        $messages = ['Hi', 'Hello', 'How Are You'];
+        $messages = [
+            'হ্যালো', 'কেমন আছেন?', 'আসসালামু আলাইকুম', // Bengali
+            'Hi', 'How are you?', 'Hello', // English
+            'مرحبا', 'كيف حالك؟', 'السلام عليكم', //  Arabic
+            'Merhaba', 'Nasılsınız?', 'Selam', // Turkish
+            'Hola', '¿Cómo estás?', 'Saludos', // Spanish
+        ];
         $this->assertContains(Greetings::greet(), $messages);
+    }
+
+    public function test_generates_random_greeting_in_local_language()
+    {
+        $messages = ['مرحبا', 'كيف حالك؟', 'السلام عليكم'];
+        $this->assertContains(Greetings::greet('ar'), $messages);
     }
 }
